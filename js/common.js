19f1b6326accb2dc77c6efa8dd3deca1855e7203
伊藤 希 <itonzm@gmail.com>
2015-07-30 23:41:13 +0900

    no message

---
+++ js/common.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('a[href^=#]').click(function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
+	
+});
