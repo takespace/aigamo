28a5f319faed3bab315399ead52d1b38914dd167
matsujirushi <matsujirushi@live.jp>
2016-07-26 21:18:12 +0900

    Arduinoと通信

--- code/Windows/AigamoControl/MainPage.xaml.cs
+++ code/Windows/AigamoControl/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -23,28 +26,55 @@ namespace AigamoControl
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private SerialDevice _SerialPort;
+
         public MainPage()
         {
             this.InitializeComponent();
         }
 
-        private void Page_Loaded(object sender, RoutedEventArgs e)
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
             // 画質が落ちるのを防ぐ措置
             var bitmapSignal = ImageSignal.Source as BitmapImage;
             bitmapSignal.DecodePixelWidth = 477;
             bitmapSignal.DecodePixelHeight = 164;
 
+            string aqs = SerialDevice.GetDeviceSelectorFromUsbVidPid(0x2A03, 0x0043);   // Arduino Uno R3
+            //string aqs = SerialDevice.GetDeviceSelectorFromUsbVidPid(0x0403, 0x6001);   // TTL-232R-5V
+            var dis = await DeviceInformation.FindAllAsync(aqs);
+            if (dis.Count <= 0) return;
+
+            _SerialPort = await SerialDevice.FromIdAsync(dis[0].Id);
+            _SerialPort.BaudRate = 9600;
+            _SerialPort.DataBits = 8;
+            _SerialPort.Parity = SerialParity.None;
+            _SerialPort.StopBits = SerialStopBitCount.One;
+            _SerialPort.Handshake = SerialHandshake.None;
+            _SerialPort.WriteTimeout = TimeSpan.FromMilliseconds(1000);
+            _SerialPort.ReadTimeout = TimeSpan.FromMilliseconds(1000);
+
             StoryboardFeet.Begin();
             Storyboard1.Begin();
         }
 
-        private void ForwardLeft_Click(object sender, RoutedEventArgs e)
+        private void Page_Unloaded(object sender, RoutedEventArgs e)
         {
+            _SerialPort?.Dispose();
         }
 
-        private void ForwardRight_Click(object sender, RoutedEventArgs e)
+        private async void ForwardLeft_Click(object sender, RoutedEventArgs e)
         {
+            var writer = new DataWriter(_SerialPort.OutputStream);
+            writer.WriteBytes(new byte[] { 0, });
+            await writer.StoreAsync();
+        }
+
+        private async void ForwardRight_Click(object sender, RoutedEventArgs e)
+        {
+            var writer = new DataWriter(_SerialPort.OutputStream);
+            writer.WriteBytes(new byte[] { 1, });
+            await writer.StoreAsync();
         }
 
         private void StopLeft_Click(object sender, RoutedEventArgs e)
@@ -62,6 +92,5 @@ namespace AigamoControl
         private void BackwordRight_Click(object sender, RoutedEventArgs e)
         {
         }
-
     }
 }
