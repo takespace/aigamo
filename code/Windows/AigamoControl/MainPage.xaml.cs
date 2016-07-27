781dd1b2bf87f20ab6fd84a45e505f65248bab23
matsujirushi <matsujirushi@live.jp>
2016-07-27 22:13:29 +0900

    Arduino通信を別クラス化

--- code/Windows/AigamoControl/MainPage.xaml.cs
+++ code/Windows/AigamoControl/MainPage.xaml.cs
@@ -1,21 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Devices.Enumeration;
-using Windows.Devices.SerialCommunication;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Storage.Streams;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
 
 // 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
 
@@ -26,7 +11,7 @@ namespace AigamoControl
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private SerialDevice _SerialPort;
+        private ArduinoDevice _Arduino;
 
         public MainPage()
         {
@@ -40,19 +25,10 @@ namespace AigamoControl
             bitmapSignal.DecodePixelWidth = 477;
             bitmapSignal.DecodePixelHeight = 164;
 
-            string aqs = SerialDevice.GetDeviceSelectorFromUsbVidPid(0x2A03, 0x0043);   // Arduino Uno R3
-            //string aqs = SerialDevice.GetDeviceSelectorFromUsbVidPid(0x0403, 0x6001);   // TTL-232R-5V
-            var dis = await DeviceInformation.FindAllAsync(aqs);
+            var dis = await ArduinoDevice.GetDeviceInformationsFromUsbVidPidAsync(ArduinoDevice.INTERFACE_ARDUINO_UNO_R3);
             if (dis.Count <= 0) return;
 
-            _SerialPort = await SerialDevice.FromIdAsync(dis[0].Id);
-            _SerialPort.BaudRate = 9600;
-            _SerialPort.DataBits = 8;
-            _SerialPort.Parity = SerialParity.None;
-            _SerialPort.StopBits = SerialStopBitCount.One;
-            _SerialPort.Handshake = SerialHandshake.None;
-            _SerialPort.WriteTimeout = TimeSpan.FromMilliseconds(1000);
-            _SerialPort.ReadTimeout = TimeSpan.FromMilliseconds(1000);
+            _Arduino = await ArduinoDevice.FromIdAsync(dis[0].Id);
 
             StoryboardFeet.Begin();
             Storyboard1.Begin();
@@ -60,21 +36,17 @@ namespace AigamoControl
 
         private void Page_Unloaded(object sender, RoutedEventArgs e)
         {
-            _SerialPort?.Dispose();
+            _Arduino?.Dispose();
         }
 
         private async void ForwardLeft_Click(object sender, RoutedEventArgs e)
         {
-            var writer = new DataWriter(_SerialPort.OutputStream);
-            writer.WriteBytes(new byte[] { 0, });
-            await writer.StoreAsync();
+            await _Arduino.Write(new byte[] { 0, });
         }
 
         private async void ForwardRight_Click(object sender, RoutedEventArgs e)
         {
-            var writer = new DataWriter(_SerialPort.OutputStream);
-            writer.WriteBytes(new byte[] { 1, });
-            await writer.StoreAsync();
+            await _Arduino.Write(new byte[] { 1, });
         }
 
         private void StopLeft_Click(object sender, RoutedEventArgs e)
