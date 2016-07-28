b9baa8fe2f6b4b2c06ee3a4cb6a9e0b5761fc743
matsujirushi <matsujirushi@live.jp>
2016-07-29 08:18:53 +0900

    速度に応じて足の回転速度を変化

--- code/Windows/AigamoControl/MainPage.xaml.cs
+++ code/Windows/AigamoControl/MainPage.xaml.cs
@@ -30,8 +30,7 @@ namespace AigamoControl
 
             _Arduino = await ArduinoDevice.FromIdAsync(dis[0].Id);
 
-            StoryboardFeet.Begin();
-            Storyboard1.Begin();
+            StoryboardAntena.Begin();
         }
 
         private void Page_Unloaded(object sender, RoutedEventArgs e)
@@ -39,5 +38,104 @@ namespace AigamoControl
             _Arduino?.Dispose();
         }
 
+        private void Forward_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeFeetSpeed(1);
+        }
+
+        private void Backward_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeFeetSpeed(-1);
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeFeetSpeed(0);
+        }
+
+        private enum FeetSpeedType
+        {
+            Stop,
+            ForwardFast,
+            ForwardSlow,
+            BackwardFast,
+            BackwardSlow,
+        }
+
+        private FeetSpeedType __FeetSpeed = FeetSpeedType.Stop;
+        private FeetSpeedType _FeetSpeed
+        {
+            get
+            {
+                return __FeetSpeed;
+            }
+            set
+            {
+                if (__FeetSpeed == value) return;
+
+                if (value == FeetSpeedType.Stop)
+                {
+                    StoryboardFeet.Stop();
+                }
+                else
+                {
+                    StoryboardFeet.Stop();
+                    switch (value)
+                    {
+                        case FeetSpeedType.ForwardFast:
+                            AnimationFootFront.To = -360;
+                            AnimationFootFront.Duration = new Duration(new System.TimeSpan(0, 0, 0, 1));
+                            AnimationFootRear.To = -360;
+                            AnimationFootRear.Duration = new Duration(new System.TimeSpan(0, 0, 0, 1));
+                            break;
+                        case FeetSpeedType.ForwardSlow:
+                            AnimationFootFront.To = -360;
+                            AnimationFootFront.Duration = new Duration(new System.TimeSpan(0, 0, 0, 3));
+                            AnimationFootRear.To = -360;
+                            AnimationFootRear.Duration = new Duration(new System.TimeSpan(0, 0, 0, 3));
+                            break;
+                        case FeetSpeedType.BackwardFast:
+                            AnimationFootFront.To = 360;
+                            AnimationFootFront.Duration = new Duration(new System.TimeSpan(0, 0, 0, 1));
+                            AnimationFootRear.To = 360;
+                            AnimationFootRear.Duration = new Duration(new System.TimeSpan(0, 0, 0, 1));
+                            break;
+                        case FeetSpeedType.BackwardSlow:
+                            AnimationFootFront.To = 360;
+                            AnimationFootFront.Duration = new Duration(new System.TimeSpan(0, 0, 0, 3));
+                            AnimationFootRear.To = 360;
+                            AnimationFootRear.Duration = new Duration(new System.TimeSpan(0, 0, 0, 3));
+                            break;
+                    }
+                    StoryboardFeet.Begin();
+                }
+
+                __FeetSpeed = value;
+            }
+        }
+
+        private void ChangeFeetSpeed(double speed)
+        {
+            if (speed == 0)
+            {
+                _FeetSpeed = FeetSpeedType.Stop;
+            }
+            else if (speed > 0.5)
+            {
+                _FeetSpeed = FeetSpeedType.ForwardFast;
+            }
+            else if (speed > 0)
+            {
+                _FeetSpeed = FeetSpeedType.ForwardSlow;
+            }
+            else if (speed < -0.5)
+            {
+                _FeetSpeed = FeetSpeedType.BackwardFast;
+            }
+            else
+            {
+                _FeetSpeed = FeetSpeedType.BackwardSlow;
+            }
+        }
     }
 }
