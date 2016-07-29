f8fb33a1cf90ad8ab6b19b32b90fe6d6b96308d3
matsujirushi <matsujirushi@live.jp>
2016-07-29 19:10:38 +0900

    通信フォーマットをアイガモ用に変更（動作未確認）

--- code/Windows/FakeAigamo/FakeAigamo.ino
+++ code/Windows/FakeAigamo/FakeAigamo.ino
@@ -3,10 +3,12 @@ void setup() {
 }
 
 void loop() {
-  int requestId;
-  if (CommTask(&requestId))
+  char command;
+  int data;
+  if (CommTask(&command, &data))
   {
-    Serial.println(requestId);
+    Serial.println(command);
+    Serial.println(data);
   }
 }
 
@@ -16,27 +18,20 @@ void CommInitialize()
   Serial.setTimeout(5000);
 }
 
-boolean CommTask(int* requestId)
+boolean CommTask(char* command, int* data)
 {
   if (Serial.available() <= 0) return false;
   if (Serial.read() != ':') return false;
   
   String message = Serial.readStringUntil('\n');
-  if (message.length() < 4 + 1 + 3) return false;
+  if (message.length() != 1 + 2) return false;
 
   int i = 0;
-  String senderId = message.substring(i, i + 2); i += 2;
-  String command = message.substring(i, i + 2); i += 2;
-  String data = message.substring(i, i + message.length() - (4 + 3)); i += message.length() - (4 + 3);
-  String footer = message.substring(i, i + 2); i += 2;
-  String delim = message.substring(i, i + 1); i += 1;
+  String c = message.substring(i, i + 1); i += 1;
+  String d = message.substring(i, i + 2); i += 2;
 
-  if (senderId != "00") return false;
-  if (command != "00") return false;
-  if (delim != "\r") return false;
-
-  if (data.length() != 2) return false;
-  if (sscanf(data.c_str(), "%x", requestId) != 1) return false;
+  *command = c[0];
+  if (sscanf(d.c_str(), "%x", data) != 1) return false;
 
   return true;
 }
